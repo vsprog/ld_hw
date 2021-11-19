1e72ae38ddd92b0dbeff65f755bab7bd97a3fc08
Vasily Shatrov <vasily_shatrov@epam.com>
2021-11-19 12:39:09 +0300

    Task 4 completion in the multithreading module.

---
+++ Multithreading/Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int times = 10;
+            int number = 100;
+
+            ThreadDelegate((number, times));
+            Console.ReadLine();
+        }
+
+        private static void ThreadDelegate(object state)
+        {
+            (int number, int times) = ((int, int))state;
+
+            if (times != 0)
+            {
+                number--;
+                Console.WriteLine(number);
+                Thread thread = new Thread(ThreadDelegate);
+                Console.WriteLine($"thread #{thread.ManagedThreadId}");
+                thread.Start((number, --times));
+                thread.Join();
+            }
+        }
+    }
+}
