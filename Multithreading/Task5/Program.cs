39327470b1c81d74bf8d3d8f8cc30941b56ab13f
Vasily Shatrov <vasily_shatrov@epam.com>
2021-11-19 12:57:19 +0300

    Task 5 completion in the multithreading module.

---
+++ Multithreading/Task5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace Task5
+{
+    class Program
+    {
+        static Semaphore sem = new Semaphore(10, 10);
+
+        static void Main(string[] args)
+        {
+            int times = 10;
+            int number = 100;
+
+            ThreadDelegate((number, times));
+            Console.ReadLine();
+        }
+
+        private static void ThreadDelegate(object state)
+        {
+            (int number, int times) = ((int, int))state;
+
+            if (times != 0)
+            {
+                sem.WaitOne();
+                
+                number--;
+                Console.WriteLine(number);
+                Console.WriteLine($"thread #{Thread.CurrentThread.ManagedThreadId}");
+                ThreadPool.QueueUserWorkItem(ThreadDelegate, (number, --times));
+
+                sem.Release();
+            }
+        }
+    }
+}
