c6702941c8f2eed70603f540ae7fdc0122e18e6e
Vasily Shatrov <vasily_shatrov@epam.com>
2021-11-25 12:45:35 +0300

    Async/await. Task 1.

---
+++ Asyncawait/Task1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number. To finish, type 'exit'.");
+
+            string input = Console.ReadLine();
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+            Action<string, CancellationToken> CountAsync = async (input, token) =>
+            {
+                string result = string.Empty;
+
+                try
+                {
+                    result = await Task.Run(() => {
+                        if (long.TryParse(input, out long number)) 
+                            return $"Result is {Count(number, token)}";
+                        return "Enter number.";
+                    }, token);
+                }
+                catch (OperationCanceledException)
+                {
+                    result = "Operation canceled.";
+                }
+
+                Console.WriteLine(result);
+            };
+
+            while (!input.Equals("exit", StringComparison.OrdinalIgnoreCase))
+            {
+                CountAsync(input, tokenSource.Token);
+                input = Console.ReadLine();
+                
+                tokenSource.Cancel();
+                tokenSource.Dispose();
+                tokenSource = new CancellationTokenSource();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static long Count(long max, CancellationToken token)
+        {
+            long result = 0;
+
+            for (long i = 0; i <= max; i++)
+            {
+                token.ThrowIfCancellationRequested();
+                result += i;
+            }
+            
+            return result;
+        }
+    }
+}
