bbeed81aadbfe3925146f7c26b12cc4bf7a20c72
vsprog <chmolekula@gmail.com>
2021-11-29 17:36:47 +0300

    Async/await. Task 3.

---
+++ Asyncawait/Task3/Startup.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Task3.Abstract;
+using Task3.Contexts;
+using Task3.Infrastructure;
+using Task3.Repositories;
+
+namespace Task3
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("db"));
+            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddScoped<Cart>(provider => SessionCart.GetCart(provider));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ Asyncawait/Task3/wwwroot/js/site.js
@@ -0,0 +1,58 @@
+﻿(function () {
+    $(window).on('load', () => {
+        $.ajax({
+            url: '/cart/GetShortModel',
+            method: 'GET',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            success: (data, textStatus, request) => {
+                updateCartInfo(data);
+            }
+        });
+    });
+
+    $('.store').on('click', (e) => {
+        var sender = $(e.target);
+        if (sender.prop('tagName').toLowerCase() !== 'button') return;
+
+        $.ajax({
+            url: '/cart/AddToCart',
+            method: 'POST',
+            data: JSON.stringify(sender.data('productId')),
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            success: (data, textStatus, request) => {
+                updateCartInfo(data);
+            }
+        });
+    });
+
+    $('.cart').on('click', (e) => {
+        var sender = $(e.target);
+        if (sender.prop('tagName').toLowerCase() !== 'button') return;
+
+        $.ajax({
+            url: '/cart/RemoveFromCart',
+            method: 'DELETE',
+            data: JSON.stringify(sender.data('productId')),
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            success: (data, textStatus, request) => {
+                sender.parent().parent().remove();
+                updateCartInfo(data);
+            }
+        });
+    });
+
+    function updateCartInfo(cartViewModel) {
+        let productCountElement = $('.session-cart_count');
+        let totalPriceElement = $('.session-cart_total-price');
+        let headerCartElement = $('.session-cart');
+
+        if (cartViewModel.totalItems == 0) headerCartElement.addClass('d-none');
+        else headerCartElement.removeClass('d-none');
+
+        productCountElement.html(cartViewModel.totalItems);
+        totalPriceElement.html(cartViewModel.totalSum);
+    }
+})()
