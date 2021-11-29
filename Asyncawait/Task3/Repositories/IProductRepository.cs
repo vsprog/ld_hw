0e73c9a352976e291d607f49f0897dc339ac95a8
vsprog <chmolekula@gmail.com>
2021-11-29 18:22:50 +0300

    Task4 blueprint

---
+++ Asyncawait/Task3/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using Task3.Models;
+
+namespace Task3.Repositories
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> GetAllProducts();
+
+        Product GetProduct(string id);
+    }
+}
---
+++ Asyncawait/Task4/Contexts/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Task4.Models;
+
+namespace Task4.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            _ = modelBuilder.Entity<User>().HasData(
+                new User 
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Qwerty",
+                    Surname = "Smith",
+                    Age = 30
+                },
+                new User
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Another one",
+                    Surname = "Smith",
+                    Age = 31
+                });
+        }
+    }
+}
