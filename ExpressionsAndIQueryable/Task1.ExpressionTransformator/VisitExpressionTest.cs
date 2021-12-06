53231d7bbe1c8a676ad87ff6a42644bc74cb5731
vsprog <chmolekula@gmail.com>
2021-12-06 18:25:48 +0300

    Parameter replacer.

--- ExpressionsAndIQueryable/Task1.ExpressionTransformator/VisitExpressionTest.cs
+++ ExpressionsAndIQueryable/Task1.ExpressionTransformator/VisitExpressionTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 
 namespace Task1.ExpressionTransformator
@@ -10,13 +11,17 @@ namespace Task1.ExpressionTransformator
         [TestMethod]
         public void TestVisit()
         {
-            Expression<Func<int, int>> source = (a) => 3 * (a - 1) * (a + 1);
-            Expression<Func<int, int>> changed = new ReplaceExpressionVisitor().VisitAndConvert(source, String.Empty);
+            var argsToReplace = new Dictionary<string, object>
+            {
+                { "a", 1 },
+                { "b", 1 }
+            };
+            Expression<Func<int, int, int>> source = (a, b) => (2 * b) + (a - 1) + (a + 1) + (a + 3) + 1;
+            Expression<Func<int, int, int>> changed = new ReplaceExpressionVisitor(argsToReplace).VisitAndConvert(source, String.Empty);
             
 
-            Console.WriteLine($"source expression: {source}\nresult: {source.Compile().Invoke(2)}");
-            Console.WriteLine($"changed expression: {changed}\nresult: {changed.Compile().Invoke(2)}");
-
+            Console.WriteLine($"source expression: {source}\nresult: {source.Compile().Invoke(2, 2)}");
+            Console.WriteLine($"changed expression: {changed}\nresult: {changed.Compile().Invoke(2, 2)}");
         }
     }
 }
