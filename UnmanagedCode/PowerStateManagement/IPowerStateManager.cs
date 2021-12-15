5ccee2956f7da6139c7bb7eff0276e7af558fb5c
Vasily Shatrov <vasily_shatrov@epam.com>
2021-12-15 18:10:50 +0300

    Task 2, 3 done.

---
+++ UnmanagedCode/PowerStateManagement/IPowerStateManager.cs
@@ -0,0 +1,21 @@
+﻿using PowerStateManagement.Settings;
+using System;
+using System.Runtime.InteropServices;
+
+namespace PowerStateManagement
+{
+    [ComVisible(true)]
+    [Guid("BA0A88F8-050F-4C17-A06E-25DA3C23E376")]
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    public interface IPowerStateManager
+    {
+        string GetLastSleepTime();
+        string GetLastWakeTime();
+        string GetSystemBatteryState();
+        string GetSystemPowerInformation();
+        void ReserveHibernateFile();
+        void DeleteHibernateFile();
+        void Hibernate();
+        void Standby();
+    }
+}
---
+++ UnmanagedCode/PowerStateManagement/reg.cmd
@@ -0,0 +1,1 @@
+﻿C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe .\bin\debug\PowerStateManagement.dll /tlb /codebase
