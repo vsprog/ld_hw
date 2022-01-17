58f982ac01afc54f11d24d5b8a1f23271f5359ff
vsprog <chmolekula@gmail.com>
2022-01-17 17:28:18 +0300

    Task 1 is done.

---
+++ Profiling/Task1.PasswordHashGenerator/Bpkdf2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+
+namespace Task1.PasswordHashGenerator
+{
+    public class Bpkdf2
+    {
+        private readonly byte[] salt;
+        private readonly byte[] passwordBytes;
+        private readonly int iterate;
+
+        public Bpkdf2(string passwordText, byte[] salt, int iterate)
+        {
+            this.salt = salt;
+            this.passwordBytes = Encoding.UTF8.GetBytes(passwordText);
+            this.iterate = iterate;
+        }
+
+        public byte[] GetBytes(int count)
+        {
+            int status = Bpkdf2Interop.BCryptOpenAlgorithmProvider(out IntPtr Alg, "SHA1", null, BCryptFlags.BCRYPT_ALG_HANDLE_HMAC_FLAG);
+
+            using (var Handle = new SafeBcryptAlgorithmHandle(Alg))
+            {
+                if (Alg != IntPtr.Zero)
+                {
+                    byte[] Result = new byte[count];
+
+                    status = Bpkdf2Interop.BCryptDeriveKeyPBKDF2(
+                        Alg,
+                        passwordBytes,
+                        passwordBytes.Length,
+                        salt,
+                        salt.Length,
+                        iterate,
+                        Result, Result.Length,
+                        BCryptFlags.NO_FLAGS);
+
+                    if (status != 0) throw new Win32Exception($"Key generator failed with NT Status {status}");
+
+                    return Result;
+                }
+                else throw new Win32Exception($"Open failed with NT Status {status}");
+            }
+        }
+    }
+}
---
+++ Profiling/Task1.PasswordHashGenerator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace Task1.PasswordHashGenerator
+{
+    class Program
+    {
+        private static readonly byte[] salt = new byte[] { 101, 78, 240, 67, 54, 187, 126, 9, 98, 164, 233, 145, 32, 228, 199, 15 };
+        private static readonly string passwordText = "qwerty123ASDF";
+        private static readonly int iterate = 10000;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"password: {passwordText}\n");
+            ShowStat(GeneratePasswordHashUsingSalt, nameof(GeneratePasswordHashUsingSalt));
+            ShowStat(FastGeneratePasswordHashUsingSalt, nameof(FastGeneratePasswordHashUsingSalt));
+            
+            Console.ReadLine();
+        }
+
+        private static string GeneratePasswordHashUsingSalt()
+        {
+            var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+            byte[] hash = pbkdf2.GetBytes(20);
+            byte[] hashBytes = new byte[36];
+
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            var passwordHash = Convert.ToBase64String(hashBytes);
+
+            return passwordHash;
+        }
+
+        private static string FastGeneratePasswordHashUsingSalt()
+        {
+            var pbkdf2 = new Bpkdf2(passwordText, salt, iterate);
+            byte[] hash = pbkdf2.GetBytes(20);
+            byte[] hashBytes = new byte[36];
+
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            var passwordHash = Convert.ToBase64String(hashBytes);
+
+            return passwordHash;
+        }
+
+        private static void ShowStat(Func<string> gereratePasswordHash, string methodName)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            string hash = gereratePasswordHash();
+            sw.Stop();
+
+            Console.WriteLine($"{methodName} time: {sw.ElapsedMilliseconds}ms");
+            Console.WriteLine($"hash: {hash}\n");
+        }
+    }
+}
